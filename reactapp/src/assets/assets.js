59e08437b0b1f512881b010b22c9f28843839afd
UdhayakumarThangavel <dev.udhayakumar@gmail.com>
2023-07-31 10:44:52 +0530

    Updated assets

--- reactapp/src/assets/assets.js
+++ reactapp/src/assets/assets.js
@@ -1,17 +1,17 @@
 export const LeaderSVG = () =>{
-    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/personal-finance-manager/main/assets/Finance%20leaders.svg"} alt={""}/>
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Finance%20leaders.svg"} alt={""}/>
 }
 export const ArrowRightIcon = () =>{
-    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/personal-finance-manager/main/assets/Arrow_right.svg"} alt={""}/>
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Arrow_right.svg"} alt={""}/>
 }
 export const AddIcon = () =>{
-    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/personal-finance-manager/main/assets/Add_round.svg"} alt={""}/>
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Add_round.svg"} alt={""}/>
 }
 
 export const AppLogo = () =>{
-    return <img style={{width:140,height:60}} src={"https://raw.githubusercontent.com/UdhayakumarThangavel/personal-finance-manager/main/assets/App%20logo.svg"} alt={""}/>
+    return <img style={{width:140,height:60}} src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/App%20logo.svg"} alt={""}/>
 }
 
 export const ArrowGIcon = () =>{
-    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/personal-finance-manager/main/assets/App%20logo.svg"} alt={""}/>
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/App%20logo.svg"} alt={""}/>
 }
